abd8b86a488e0dac888f2b8bb9b8d1a8af361973
Dalubuhle-Dlamini <68609913+Dalubuhle-Dlamini@users.noreply.github.com>
2021-05-29 10:18:31 +0100

    Update form.js

--- form.js
+++ form.js
@@ -1,37 +1,58 @@
 //THIS WILL CHANGE BORDER COLOR WHEN IT IS FOCUSED ON
+
+
 setInterval( () => {
     //VARIABLES
-    let div1 =document.querySelector("#div1") //DIV1
-    let div2 =document.querySelector("#div2") //DIV2
-    let div3 =document.querySelector("#div3") //DIV3
+    let active = document.activeElement;
     let div = document.querySelector("#div"); //DIV
     let input = document.querySelector("#input"); //INPUT
-    let input1 = document.querySelector("#input1"); //INPUT1
-    let input2 = document.querySelector("#input2"); //INPUT2
-    let input3 = document.querySelector("#input3"); //INPUT3
-    let active = document.activeElement;
 
     //IF STATEMENT
     if(input === active){
         div.style.border = "solid 1px red";
         div.style.transition = "2s"
 
-    }else if(input1 === active){
+    }else{div.style.border = "solid 1px white"}
+}, 500)
+
+setInterval( () => {
+    let active = document.activeElement;
+    let input1 = document.querySelector("#input1");
+    let div1 =document.querySelector("#div1")
+
+    if(input1 === active){
         div1.style.border = "solid 1px red";
         div1.style.transition = "2s"
 
-    }else if(input2 === active){
+    }else{div1.style.border = "solid 1px white"}
+}, 500)
+
+setInterval( () => {
+    let active = document.activeElement;
+    let input2 = document.querySelector("#input2");
+    let div2 =document.querySelector("#div2")
+
+    if(input2 === active){
         div2.style.border = "solid 1px red";
         div2.style.transition = "2s"
 
-    }else if(input3 === active){
+    }else{div2.style.border = "solid 1px white"}
+}, 500)
+
+setInterval( () => {
+    let active = document.activeElement;
+    let input3 = document.querySelector("#input3");
+    let div3 =document.querySelector("#div3")
+
+    if(input3 === active){
         div3.style.border = "solid 1px red";
         div3.style.transition = "2s"
 
-    }else if((input && input1 && input2 && input3) !== active){
-        div.style.border = "solid 1px white"
-
-    }
+    }else{div3.style.border = "solid 1px white"}
 }, 500)
+/*here i made the code long because if i made an else if statement the
+result/output did not give me what i wanted if you have a solution to this 
+problem please let me know*/
+
 
 //PASSWORD VALIDATION
